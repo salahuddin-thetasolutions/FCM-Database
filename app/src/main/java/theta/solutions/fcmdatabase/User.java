3c9d8af9212dac51ea2050be98052eb3cdaf6576
HAFIZ SALAH UD DIN <salahudinhafiz@hotmail.com>
2018-05-14 17:54:39 +0500

    Firebase DataBase + FCM Lecture

---
+++ app/src/main/java/theta/solutions/fcmdatabase/User.java
@@ -0,0 +1,24 @@
+package theta.solutions.fcmdatabase;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by ThetaTeam2 on 09/04/2018.
+ */
+
+@IgnoreExtraProperties
+public class User {
+
+    public String name;
+    public String email;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
